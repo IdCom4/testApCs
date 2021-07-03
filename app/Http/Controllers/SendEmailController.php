705eb9aa3e119adf5c68830c42366ee72706c4d0
Idriss Cornuau <cornuau.idriss@gmail.com>
2021-07-03 17:42:39 +0200

    mail error handling

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -31,8 +31,26 @@ class SendEmailController extends Controller
         "message" => $request->message,
       );
 
-      Mail::to("reservations@au-petit-chez-soi.fr")->send(new SendMail($data));
-      
+      /*$data = array(
+        "nom" => "De: " . $request->nom,
+        "mail" => "\nAdresse Mail: " . $request->mail,
+        "tel" => "\nTel: " . $request->tel,
+        "personnes" => "\nNombre de personnes: " . $request->personnes,
+        "jour" => "\nLe: " . $request->jour,
+        "message" => "\n\n" . $request->message,
+      );
+
+      if (mail("reservations@au-petit-chez-soi.fr", "Mail de réservation", $data["nom"] . $data["mail"] . $data["tel"] . $data["personnes"] . $data["jour"] . $data["message"]))
+        echo ("Mail envoyé");
+      else
+        echo ("Erreur lors de l'envoie du mail");*/
+
+      try {
+        Mail::to("reservations@au-petit-chez-soi.fr")->send(new SendMail($data));
+      } catch(\Exception $e) {
+        return (back()->withErrors(["Notre serveur est surchargé, votre demande n'a pas pu être envoyée.", "Veuillez réessayer plus tard ou nous contacter directement."]));
+      }
+
       return (back()->with("success", "Le mail à bien été envoyé !"));
     }
 }
